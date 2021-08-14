35328cd210ad2bcd38cb0fe18e9fc4fe6b0fe0b8
Tobetch <takutobe@gmail.com>
2021-08-14 14:45:31 +0900

    Initial Commit

---
+++ src/main/java/com/example/raisetechjava202107/RaisetechJava202107Application.java
@@ -0,0 +1,13 @@
+package com.example.raisetechjava202107;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaisetechJava202107Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaisetechJava202107Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/raisetechjava202107/RaisetechJava202107ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raisetechjava202107;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaisetechJava202107ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
